45f4012a33a560fcdd322e522128887479647ce5
joshuasherman-macys <joshua.sherman@macys.com>
2026-03-04 12:40:23 -0500

    Update database.py to use private iptype

--- backend/src/database.py
+++ backend/src/database.py
@@ -113,7 +113,7 @@ async def get_connection():
         user=config_service.DB_USER,
         password=config_service.DB_PASS,
         db=config_service.DB_NAME,
-        ip_type=IPTypes.PUBLIC,  # Adjust if using Private IP
+        ip_type=IPTypes.PRIVATE,  # Adjust if using Private IP
     )
         
     return conn
@@ -176,7 +176,7 @@ class WorkerDatabase:
                     user=config_service.DB_USER,
                     password=config_service.DB_PASS,
                     db=config_service.DB_NAME,
-                    ip_type=IPTypes.PUBLIC,
+                    ip_type=IPTypes.PRIVATE,
                 )
 
             self.engine = create_async_engine(
